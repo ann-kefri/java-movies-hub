9a4d03916b5d66c136527491448fcd9249da45ee
annkefri <annkefri@yandex.ru>
2026-06-29 22:48:06 +0300

    Исправить стиль кода

--- src/test/ru/practicum/moviehub/http/MoviesApiTest.java
+++ src/test/ru/practicum/moviehub/http/MoviesApiTest.java
@@ -14,7 +14,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.SplittableRandom;
 
 import static org.junit.jupiter.api.Assertions.*;
 
